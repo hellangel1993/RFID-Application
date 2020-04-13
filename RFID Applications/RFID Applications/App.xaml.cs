ad4f9dc5131abbb93ae9985612889d2d8c95a5eb
Himanshu Chowdhary <himanshu.chowdhary@qvantel.com>
2020-04-13 22:41:48 +0530

    Add project files.

---
+++ RFID Applications/RFID Applications/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using RFID_Applications.Services;
+using RFID_Applications.Views;
+
+namespace RFID_Applications
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
